925256a6ed5ff4ef60179bc290e8051fb3f2a5ae
Karla Patricia G. Fariñas <karlafarinas@gmail.com>
2025-08-06 00:23:40 +0800

    Completed my 3 simple placeholder tests.

--- vite.test.jsx
+++ vite.test.jsx
@@ -2,9 +2,12 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import DataProvider from './src/context/DataProvider';
 import Login from './src/pages/Login/Login';
+import Message from './src/pages/Message/Message';
+import Signup from './src/pages/Login/SignUp';
 
-describe('Login Component', () => {
-  test('displays email input with correct placeholder', () => {
+
+describe('Component Tests', () => {
+  test('Login displays email input with correct placeholder', () => {
     render(
       <DataProvider>
         <MemoryRouter initialEntries={['/login']}>
@@ -18,5 +21,33 @@ describe('Login Component', () => {
 
     const emailInput = screen.getByPlaceholderText(/Your e-mail/i);
     expect(emailInput).toBeInTheDocument();
+  });
+
+  test('Message component renders textarea with correct placeholder', () => {
+    render(
+      <DataProvider>
+        <Message
+          receiverId={1}
+          receiverEmail="test@email.com"
+          messages={[]}
+          setMessages={() => {}}
+        />
+      </DataProvider>
+    );
+
+    const textarea = screen.getByPlaceholderText(/Message test@email.com.../i);
+    expect(textarea).toBeInTheDocument();
+  });
+
+test('Signup displays password input with correct placeholder', () => {
+  render(
+    <MemoryRouter initialEntries={['/signup']}>
+      <Signup />
+    </MemoryRouter>
+  );
+
+  const passwordInput = screen.getByPlaceholderText(/Enter a password/i);
+  expect(passwordInput).toBeInTheDocument();
+
   });
 });
